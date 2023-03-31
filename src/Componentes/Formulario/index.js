11050e7001758bba5ec4cf19500e02f4265e1f54
Fredson <fredsondev@gmail.com>
2023-03-31 18:49:48 -0300

    cria componente botao adicionando validacoes no form

--- src/Componentes/Formulario/index.js
+++ src/Componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css'
@@ -13,14 +14,23 @@ const Formulario = () => {
     'Mobile',
     'Inovação e Gestão'
   ]
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    console.log('Ao Salvar');
+  }
+
   return (
     <section className='formulario'>
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador.</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa label="Times" itens={times} />
+        <ListaSuspensa obrigatorio={true} label="Times" itens={times} />
+        <Botao>
+          Criar Card
+        </Botao>
       </form>
     </section>
   )
