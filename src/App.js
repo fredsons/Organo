99bbea157eefd6c96104d916c666ed071185940a
Fredson <fredsondev@gmail.com>
2023-03-31 15:35:50 -0300

    cria componente de texto/input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Banner from './Componentes/Banner/Banner';
+import Banner from './Componentes/Banner';
+import CampoTexto from './Componentes/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto label="Nome" placeholder="Digite seu nome" />
+      <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
     </div>
   );
 }
