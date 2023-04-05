77d72d56c7150611d24f27f8cc6c36d5484d9dfb
Fredson <fredsondev@gmail.com>
2023-04-05 17:47:25 -0300

    atualizacao

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import Banner from './Componentes/Banner';
 import Formulario from './Componentes/Formulario';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoNovoColaboradorAdicionado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
     </div>
   );
 }
